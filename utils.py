a81dc90aedb415386b59443ff30b419c77b703ef
yuliakachurovska <kachurovska167kv@gmail.com>
2024-02-13 13:10:29 +0200

    Add file degree of five

--- utils.py
+++ utils.py
@@ -3,3 +3,13 @@ def factorial(n):
 		return 1
 	else:
 		return n * factorial(n - 1)
+
+
+def degree_of_five(numb):
+    if numb <= 0:
+        return False
+    elif numb == 1:
+        return True
+    while numb % 5 == 0:
+        numb /= 5
+    return numb == 1
