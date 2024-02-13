03b1f02f6b3fbff092a4ea38da66050aef7a0f12
yuliakachurovska <kachurovska167kv@gmail.com>
2024-02-13 13:44:45 +0200

    new changes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from utils import factorial, is_prime, gcd, degree_of_five
 print(factorial(5))
 print(is_prime(3))
-print(gcd(8, 12))
-number = int(input("Введіть число: "))
-print(degree_of_five(number))
+print(degree_of_five(125))
